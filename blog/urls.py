21b2ad4ecff96a1aa40316b048a6e029929ff85d
Fernando <furdac14@gmail.com>
2020-05-31 20:05:26 -0400

    Add login functionality with error TemplateDoesNotExist at /accounts/login/ django/forms/widgets/text.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -8,4 +10,5 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/draft/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
