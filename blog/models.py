76f0a8c5d5b388685e18be586faa725f1320291f
Fernando <furdac14@gmail.com>
2020-06-01 21:03:22 -0400

    Add feature to add comments to posts

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,18 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_date = timezone.now
         self.save()
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    # models.ForeignKey('path.to.the.model', ...)
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.text
